4b6d13feaf8252cfc0c064d8e8f9f323e5e75b37
pandecx <cpandey.94@gmail.com>
2021-01-17 14:47:03 +0530

    Changes for Inheritance

--- src/com/chetan/java/InheritanceDemo.java
+++ src/com/chetan/java/InheritanceDemo.java
@@ -1,4 +1,19 @@
 package com.chetan.java;
 
+class Manager{
+    int salary=100000;
+}
+
+class Employee extends Manager{
+    int id=1;
+    //String name;
+    int empSal=25000;
+
+}
 public class InheritanceDemo {
+    public static void main(String[] args){
+        Employee emp=new Employee();
+        System.out.println("Employee with id "+emp.id+" gets salary of "+emp.empSal);
+        System.out.println("Manager salary is "+emp.salary);
+    }
 }
