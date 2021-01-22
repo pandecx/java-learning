3165271d08b45d9330741388303d375910be4a08
pandecx <cpandey.94@gmail.com>
2021-01-22 14:49:17 +0530

    Thread Sync Demo

--- src/com/chetan/javaadvanced/MultiThreadRunnable.java
+++ src/com/chetan/javaadvanced/MultiThreadRunnable.java
@@ -1,7 +1,9 @@
 package com.chetan.javaadvanced;
 
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 public class MultiThreadRunnable {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Runnable obj1 = new Runnable() {
             @Override
@@ -12,26 +14,21 @@ public class MultiThreadRunnable {
                 }
             }
         };
-
         //Via Lambda Expression
-
         Runnable obj2 = ()-> {
             for(int i=0;i<=5;i++) {
                 System.out.println("Hello");
                 try{Thread.sleep(500);}catch(Exception e){}
             }
         };
-
         Thread t1 = new Thread(obj1);
         Thread t2 = new Thread(obj2);
-
         Thread t3=new Thread(()-> {
             for(int i=0;i<=5;i++) {
                 System.out.println("Not Runnable Object");
                 try{Thread.sleep(500);}catch(Exception e){}
             }
         });
-
         t1.start();
         try {
             Thread.sleep(100);
@@ -43,5 +40,11 @@ public class MultiThreadRunnable {
         } catch (Exception e) {
         }
         t3.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+
+        System.out.println("Bye..!!");
     }
 }
